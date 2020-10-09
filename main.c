92f356bc2537028087d3737d852b4ac0953d3fd3
99002511 <72496803+99002511@users.noreply.github.com>
2020-10-09 06:39:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,9 @@
 #include "sample.h"
 int main()
 {
-    int num1=0,num2=0,z;
-    printf("Enter first number");
-    scanf("%d",&num1);
-    printf("Enter second number");
-    scanf("%d",&num2);
-    z=add(num1,num2);
-    printf("%d",z);
+    printf("simple program for helloworld");
+    
+    add();
+  
     return 0;
 }
